77eac169de259d9cad5a3911525a58d0959c4341
suragch <studymongolian@gmail.com>
2022-02-18 11:03:38 +0800

    Create cin.cpp

---
+++ week_1/cin.cpp
@@ -0,0 +1,12 @@
+// types
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    string name;
+    cout << "What is your name? ";
+    cin >> name;
+
+    cout << "Hello, " << name << "\n";
+}
