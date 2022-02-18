64ad50611dba76af1eba25f66b9453e3d82e0116
suragch <studymongolian@gmail.com>
2022-02-18 10:49:12 +0800

    Create types.cpp

---
+++ week_1/types.cpp
@@ -0,0 +1,22 @@
+// types
+#include <iostream>
+using namespace std;
+int main() 
+{
+    // integer
+    int a = 7;
+    cout << a << "\n";
+
+    // double
+    double c = 5.4;
+    cout << c << "\n";
+
+    // strings
+    string e = "hello";
+    cout << e << "\n";
+
+    // Boolean
+    bool g = true;
+    cout << g << "\n";
+}
+
